3e020fa98b20698d274e132c692b5950afcc0d38
Ryan A <ravenengineerryan@gmail.com>
2018-08-02 05:42:39 -0500

    Pushed all files

---
+++ redditApi/redditAPI.py
@@ -0,0 +1,82 @@
+from praw.models import MoreComments
+import pandas as pd
+import numpy as np
+import webbrowser
+import urllib.request
+import praw
+import json
+import sys
+import re
+get_ipython().magic('matplotlib inline')
+
+time = (sys.argv[0],sys.argv[1])
+
+time = (10,9)
+
+com_freq = "https://api.pushshift.io/reddit/search/comment/?q=vodafone&aggs=created_utc&after={0}d&before={1}d&frequency=hour&size=50".format(10,9)
+
+sub_freq = "https://api.pushshift.io/reddit/search/submission/?q=vodafone&aggs=created_utc&after={0}d&before={1}d&frequency=hour&size=50".format(10,9)
+
+print(com_freq)
+
+df_com = pd.DataFrame({'created_utc':0,'text':""},index=[0])
+
+df_sub = pd.DataFrame({'created_utc':0,'text':"",'body':""},index=[0])
+
+cols = ["created_utc",'time']
+
+with urllib.request.urlopen(com_freq) as url:
+    data = json.loads(url.read().decode())
+
+    data1 = data['data']
+
+    data2 = data['aggs']['created_utc'] 
+    
+    cur_ind = 0
+    cur_count = 0
+    for i in range(len(data1)):
+        if( cur_count == data2[cur_ind]['doc_count']):
+            cur_count = 0
+            cur_ind +=1
+
+        time_list = {}
+        time_list['created_utc'] = data2[cur_ind]['key']
+        time_list['text'] = data1[i]['body']
+        
+        cur_count+=1
+
+        df = pd.DataFrame(time_list,index = [0])
+        df_com = df_com.append(df,cols)
+
+with urllib.request.urlopen(sub_freq) as url:
+    data = json.loads(url.read().decode())
+
+    data1 = data['data']
+    
+    data2 = data['aggs']['created_utc']
+    
+    cur_ind = 0
+    cur_count = 0
+    for i in range(len(data1)):
+        if( cur_count == data2[cur_ind]['doc_count']):
+            cur_count = 0
+            cur_ind +=1
+
+        time_list = {}
+        time_list['created_utc'] = data1[cur_ind]['created_utc']
+        time_list['text'] = data1[i]['title']
+        time_list['body'] = data1[i]['selftext']
+        
+        cur_count+=1
+
+        df = pd.DataFrame(time_list,index = [0])
+        df_sub = df_sub.append(df,cols)
+
+df_com = df_com.iloc[1:]
+
+df_sub = df_sub.iloc[1:]
+
+df_com.to_csv('reddit_com.csv',header=False,index=False,mode='a')
+
+df_sub.to_csv('reddit_sub.csv',header=False,index=False,mode='a')
+
---
+++ twitterApi/GetOldTweets-python-master/TwitterApi.py
@@ -0,0 +1,53 @@
+from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+from datetime import datetime
+import time
+import tweepy
+import pyquery
+import datetime
+import time
+import lxml
+import sys
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+import sys,getopt,datetime,codecs
+if sys.version_info[0] < 3:
+    import got
+else:
+    import got3 as got
+
+tweetsCriteria = got.manager.TweetCriteria()
+
+tweetsCriteria = tweetsCriteria.setMaxTweets(1000)
+
+tweetsCriteria = tweetsCriteria.setQuerySearch("Vodafone")
+
+tweetsCriteria = tweetsCriteria.setSince(sys.argv[1])
+
+tweetsCriteria = tweetsCriteria.setUntil(sys.argv[2])
+
+tweets = got.manager.TweetManager.getTweets(tweetsCriteria)
+
+file = open("vdf_tweets.txt","a")
+
+tweet_dict = {}
+for tweet in tweets:
+    year = tweet.date.year
+    month = tweet.date.month
+    day = tweet.date.day
+    
+    date = str(year)+"-"+str(month)+"-"+str(day)
+
+    date = datetime.datetime.strptime(date,'%Y-%m-%d')
+    date = time.mktime(date.timetuple())
+
+    ID = tweet.id
+    txt = tweet.text
+    tweet_dict[ID]=txt
+    file.write(str(date)+" "+str(txt)+"\n")
+
+file.close()
+
